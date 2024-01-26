b984570b5a5a263150fe78259c69b32ad21a2012
zhouyang <pointer19910926@gmail.com>
2024-01-26 21:15:09 +0800

    v0.1.126

--- src/renderer/services/address.ts
+++ src/renderer/services/address.ts
@@ -1,7 +1,7 @@
 import { AddressActivityFormat, AddressActivityVO, POST, PageQueryDTO, PageResponseVO } from ".";
 
 export async function fetchAddressActivity( params : { address:string , blockNumberStart : number , blockNumberEnd : number } | PageQueryDTO ) : Promise<PageResponseVO<AddressActivityVO>> {
-  const serverResponse = await POST( `http://10.0.0.172:5005/address/activity` , params );
+  const serverResponse = await POST( `http://47.107.47.210:5005/address/activity` , params );
   console.log("fetch result:" , serverResponse.data.records)
   serverResponse.data.records = serverResponse.data.records.map(
     AddressActivityFormat
