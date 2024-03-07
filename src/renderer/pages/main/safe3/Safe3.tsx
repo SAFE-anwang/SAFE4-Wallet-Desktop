a6e3f03e5ef32f8b82a137eda71d95f53e5faccb
zhouyang <pointer19910926@gmail.com>
2024-03-07 21:25:36 +0800

    v0.1.307

--- src/renderer/pages/main/safe3/Safe3.tsx
+++ src/renderer/pages/main/safe3/Safe3.tsx
@@ -5,12 +5,20 @@ import { message, Steps, theme } from 'antd';
 import { useEffect, useState } from "react";
 import Safe3PrivateKey from "../../../utils/Safe3PrivateKey";
 
-
 const { Text, Title } = Typography;
 
+const base58Regex = /^[1-9A-HJ-NP-Za-km-z]{52}$/;
+const isSafe3DesktopExportPrivateKey = (input: string) => {
+  return base58Regex.test(input);
+};
 
 export default () => {
+
   const [safe3PrivateKey, setSafe3PrivateKey] = useState<string>();
+  const [inputErrors, setInputErrors] = useState<{
+    safe3PrivateKeyError: string
+  }>();
+
   const [safe3Wallet, setSafe3Wallet] = useState<{
     privateKey: string,
     publicKey: string,
@@ -21,9 +29,26 @@ export default () => {
   }>();
 
   useEffect(() => {
-    const result = Safe3PrivateKey("XJ2M1PbCAifB8W91hcHDEho18kA2ByB4Jdmi4XBHq5sNgtuEpXr4");
-    console.log("result >> :", result);
-  }, [])
+    // const result = Safe3PrivateKey("XJ2M1PbCAifB8W91hcHDEho18kA2ByB4Jdmi4XBHq5sNgtuEpXr4");
+    console.log("safe3 private key >> :", safe3PrivateKey);
+    if (safe3PrivateKey) {
+      if (isSafe3DesktopExportPrivateKey(safe3PrivateKey)) {
+        setInputErrors(undefined);
+        setSafe3Wallet(
+          Safe3PrivateKey(safe3PrivateKey)
+        )
+      } else {
+        setInputErrors({
+          safe3PrivateKeyError: "请输入从 Safe3 桌面钱包导出的私钥"
+        })
+        setSafe3Wallet(undefined);
+      }
+    } else {
+      setInputErrors(undefined);
+      setSafe3Wallet(undefined);
+    }
+  }, [safe3PrivateKey]);
+
   return (<>
 
     <Row style={{ height: "50px" }}>
@@ -38,31 +63,83 @@ export default () => {
       <div style={{ margin: "auto", width: "90%" }}>
         <Card>
           <Alert showIcon type="info" message={<>
-            <Text>使用Safe3网络的钱包地址的私钥对信息进行签名</Text><br />
-            <Text>验证通过的签名信息会将Safe3网络的锁仓置换到Safe4网络的钱包地址上</Text>
+            <Text>使用该页面输入钱包私钥将 Safe3 网络的资产迁移到 Safe4 网络</Text><br />
           </>} />
-          <Row style={{marginTop:"50px"}}>
+          <Row style={{ marginTop: "20px" }}>
             <Col span={24}>
               <Text strong type="secondary">Safe3 钱包私钥</Text>
             </Col>
             <Col span={24} style={{ marginTop: "5px" }}>
-              <Input size="large" />
+              <Input size="large" onChange={(event) => {
+                const value = event.target.value;
+                if (isSafe3DesktopExportPrivateKey(value)) {
+                  setSafe3PrivateKey(value);
+                } else {
+                  setSafe3PrivateKey(undefined);
+                }
+              }} onBlur={(event) => {
+                const value = event.target.value;
+                setSafe3PrivateKey(value);
+              }} />
             </Col>
-            <Col span={24} style={{ marginTop: "5px" }}>
-              <Alert type="error" showIcon message={<>
-                请输入从 Safe 桌面钱包导出的私钥地址
+            {
+              inputErrors && inputErrors.safe3PrivateKeyError && <>
+                <Col span={24} style={{ marginTop: "5px" }}>
+                  <Alert type="error" showIcon message={<>
+                    {inputErrors.safe3PrivateKeyError}
+                  </>} />
+                </Col>
+              </>
+            }
+
+            <Divider />
+
+            <Col span={12}>
+              <Col span={24}>
+                <Text strong type="secondary">Safe3 钱包地址</Text><br />
+              </Col>
+              <Col span={24}>
+                <Text strong>{safe3Wallet?.safe3Address}</Text><br />
+              </Col>
+              <Col span={24} style={{ marginTop: "5px" }}>
+                <Text strong type="secondary">账户余额</Text><br />
+              </Col>
+              <Col span={24}>
+                <Text strong>213.213 SAFE</Text><br />
+              </Col>
+              <Col span={24} style={{ marginTop: "5px" }}>
+                <Text strong type="secondary">锁仓余额</Text><br />
+              </Col>
+              <Col span={24}>
+                <Text strong>213.213 SAFE</Text><br />
+              </Col>
+              <Col span={24} style={{ marginTop: "5px" }}>
+                <Text strong type="secondary">主节点</Text><br />
+              </Col>
+              <Col span={24}>
+                <Text strong>213.213 SAFE</Text><br />
+              </Col>
+            </Col>
+
+            <Col span={12}>
+              <Text strong type="secondary">Safe3 钱包地址 [公钥压缩]</Text><br />
+              <Col span={24}>
+                <Text strong>{safe3Wallet?.safe3CompressAddress}</Text><br />
+              </Col>
+            </Col>
+            <Divider />
+            <Col span={24}>
+              <Alert type="info" showIcon message={<>
+                您在 Safe3 网络的资产将会迁移到该私钥对应的 Safe4 网络的钱包地址上
               </>} />
+              <Col span={24} style={{ marginTop: "20px" }}>
+                <Text strong type="secondary">Safe4 钱包地址</Text><br />
+              </Col>
+              <Col span={24}>
+                <Text strong>{safe3Wallet?.safe4Address}</Text><br />
+              </Col>
+              <Button style={{ marginTop: "20px" }} type="primary">迁移</Button>
             </Col>
-            <Divider />
-            <Col span={12}>
-              <Text strong type="secondary">Safe3 钱包地址</Text><br />
-              dsads
-            </Col>
-            <Col span={12}>
-              <Text strong type="secondary">Safe4 钱包地址</Text><br />
-              dsads
-            </Col>
-            <Divider />
           </Row>
 
         </Card>
