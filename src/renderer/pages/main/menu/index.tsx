2c4505f829d218ee157f912a4c3233ef4ec1e4d4
zhouyang <pointer19910926@gmail.com>
2024-09-30 21:28:31 +0800

    v1.0.3-0930

--- src/renderer/pages/main/menu/index.tsx
+++ src/renderer/pages/main/menu/index.tsx
@@ -40,7 +40,7 @@ export default () => {
               <DatabaseOutlined />
             </Col>
             <Col span={20}>
-              版本 v1.0.2
+              版本 v1.0.3
             </Col>
             {/* <Col span={2} style={{ textAlign: "center" }}>
               <RightOutlined />
