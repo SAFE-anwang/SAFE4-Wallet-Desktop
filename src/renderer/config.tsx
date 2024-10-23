115e8f849c739703544ac322459c313b97429012
zhouyang <pointer19910926@gmail.com>
2024-10-23 16:50:12 +0800

    v1.0.6-023

--- src/renderer/config.tsx
+++ src/renderer/config.tsx
@@ -7,15 +7,15 @@ export const Safe4_Network_Config = {
     // endpoint: "http://47.107.47.210:8545",
     // Safescan_URL: "http://47.107.47.210",
     // Safescan_Api: "http://47.107.47.210:5005"
-    endpoint: "https://safe4-testnet.anwang.com/rpc",
-    Safescan_URL: "https://safe4-testnet.anwang.com",
-    Safescan_Api: "https://safe4-testnet.anwang.com/5005"
+    endpoint: "https://safe4testnet.anwang.com/rpc",
+    Safescan_URL: "https://safe4testnet.anwang.com",
+    Safescan_Api: "https://safe4testnet.anwang.com/5005"
   },
   Mainnet: {
-    chainId: 666666,
-    endpoint: "https://safe4.anwang.com/rpc",
-    Safescan_URL: "https://safe4.anwang.com",
-    Safescan_Api: "https://safe4.anwang.com/5005"
+    chainId: 6666666,
+    endpoint: "https://safe4testnet.anwang.com/rpc",
+    Safescan_URL: "https://safe4testnet.anwang.com",
+    Safescan_Api: "https://safe4testnet.anwang.com/5005"
   }
 }
 
@@ -52,8 +52,8 @@ const Config = {
   "test": {
     Default_Web3_Endpoint: Safe4_Network_Config.Testnet.endpoint,
     Default_Web3_ChainId: Safe4_Network_Config.Testnet.chainId,
-    Safescan_URL:  Safe4_Network_Config.Testnet.Safescan_URL,
-    Safescan_Api:  Safe4_Network_Config.Testnet.Safescan_Api
+    Safescan_URL: Safe4_Network_Config.Testnet.Safescan_URL,
+    Safescan_Api: Safe4_Network_Config.Testnet.Safescan_Api
   },
   "prod": {
     Default_Web3_Endpoint: Safe4_Network_Config.Mainnet.endpoint,
