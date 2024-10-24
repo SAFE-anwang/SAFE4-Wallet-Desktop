387f103db6d202d43924a88bb8105b744ba78bab
ZhouYang <pointer19910926@gmail.com>
2024-10-25 00:48:28 +0800

    v2.0.0-025

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,26 +2,24 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
+
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import store from './state';
-import './i18n';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
     </Provider>
   </React.StrictMode>
 );
 
-// calling IPC exposed from preload script
-// window.electron.ipcRenderer.once('ipc-example', (arg) => {
-//   // eslint-disable-next-line no-console
-//   console.log(arg);
-// });
-// window.electron.ipcRenderer.sendMessage('ipc-example', ['ping']);
+
 
 
