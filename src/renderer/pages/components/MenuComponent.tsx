8f0c0b321798ece61b16022387f9e58017415efa
ZhouYang <pointer19910926@gmail.com>
2025-11-10 00:25:10 +0800

    menuupdate

--- src/renderer/pages/components/MenuComponent.tsx
+++ src/renderer/pages/components/MenuComponent.tsx
@@ -12,6 +12,8 @@ import { useDispatch } from 'react-redux';
 import { applicationUpdateWalletUpdateVersion } from '../../state/application/action';
 import VersionModal from '../main/menu/version/VersionModal';
 import { useUnreadProposalIds } from '../../state/proposals/hooks';
+import { useWeb3React } from '@web3-react/core';
+import { Safe4NetworkChainId } from '../../config';
 
 const { Text } = Typography;
 type MenuItem = Required<MenuProps>['items'][number];
@@ -36,45 +38,71 @@ const MenuComponent: React.FC = () => {
   const [current, setCurrent] = useState('/main/wallet');
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const { chainId } = useWeb3React();
   const { API } = useSafeScan();
   const platform = useApplicationPlatform();
   const blockNumber = useBlockNumber();
   const dispatch = useDispatch();
   const unreadProposalIds = useUnreadProposalIds();
 
-
   const items: MenuItem[] = useMemo(() => {
-
-    return [
-      getItem(t("wallet"), '/main/wallet', <WalletOutlined />),
-      getItem(t("supernode"), '/main/supernodes', <ClusterOutlined />),
-      getItem(t("masternode"), '/main/masternodes', <ApartmentOutlined />),
-
-      /////
-      getItem(
-        <>
-          {t("proposal")}
-          {
-            unreadProposalIds && unreadProposalIds.length > 0 && <>
-              <Tooltip title="未查看的提案">
-                <Text style={{ marginLeft: "5px" }} type='secondary'>({unreadProposalIds?.length})</Text>
-              </Tooltip>
-            </>
-          }
-        </>,
-        '/main/proposals', <FilePptOutlined />),
-      ////
-
-      getItem(t("safe3AssetRedeem"), '/main/safe3nav', <ApiOutlined />),
-      getItem(t("wallet_crosschain"), '/main/crosschain', <SyncOutlined />),
-      getItem(t("wallet_safeswap"), '/main/swap', <SwapOutlined />),
-      getItem("流动性挖矿", '/main/farm', <ExperimentOutlined />),
-      getItem(t("wallet_issue"), '/main/issue', <BankOutlined />),
-      getItem(t("contract"), '/main/contracts', <FileZipOutlined />),
-      getItem(t("getTestCoin"), '/main/gettestcoin', <GiftOutlined />),
-      // getItem(t("batchSyncNode"), '/main/batchSyncNode', <GiftOutlined />),
-    ]
-  }, [t, unreadProposalIds]);
+    if ( chainId == Safe4NetworkChainId.Testnet ){
+      // 测试网菜单
+      return [
+        getItem(t("wallet"), '/main/wallet', <WalletOutlined />),
+        getItem(t("supernode"), '/main/supernodes', <ClusterOutlined />),
+        getItem(t("masternode"), '/main/masternodes', <ApartmentOutlined />),
+        /////
+        getItem(
+          <>
+            {t("proposal")}
+            {
+              unreadProposalIds && unreadProposalIds.length > 0 && <>
+                <Tooltip title="未查看的提案">
+                  <Text style={{ marginLeft: "5px" }} type='secondary'>({unreadProposalIds?.length})</Text>
+                </Tooltip>
+              </>
+            }
+          </>,
+          '/main/proposals', <FilePptOutlined />),
+        ////
+        getItem(t("safe3AssetRedeem"), '/main/safe3nav', <ApiOutlined />),
+        getItem(t("wallet_crosschain"), '/main/crosschain', <SyncOutlined />),
+        getItem(t("wallet_safeswap"), '/main/swap', <SwapOutlined />),
+        getItem("流动性挖矿", '/main/farm', <ExperimentOutlined />),
+        getItem(t("wallet_issue"), '/main/issue', <BankOutlined />),
+        getItem(t("contract"), '/main/contracts', <FileZipOutlined />),
+        getItem(t("getTestCoin"), '/main/gettestcoin', <GiftOutlined />),
+      ]
+    } else {
+      // 主网菜单
+      return [
+        getItem(t("wallet"), '/main/wallet', <WalletOutlined />),
+        getItem(t("supernode"), '/main/supernodes', <ClusterOutlined />),
+        getItem(t("masternode"), '/main/masternodes', <ApartmentOutlined />),
+        /////
+        getItem(
+          <>
+            {t("proposal")}
+            {
+              unreadProposalIds && unreadProposalIds.length > 0 && <>
+                <Tooltip title="未查看的提案">
+                  <Text style={{ marginLeft: "5px" }} type='secondary'>({unreadProposalIds?.length})</Text>
+                </Tooltip>
+              </>
+            }
+          </>,
+          '/main/proposals', <FilePptOutlined />),
+        ////
+        getItem(t("safe3AssetRedeem"), '/main/safe3nav', <ApiOutlined />),
+        getItem(t("wallet_crosschain"), '/main/crosschain', <SyncOutlined />),
+        getItem(t("wallet_safeswap"), '/main/swap', <SwapOutlined />),
+        getItem(t("wallet_issue"), '/main/issue', <BankOutlined />),
+        getItem(t("contract"), '/main/contracts', <FileZipOutlined />),
+        getItem(t("getTestCoin"), '/main/gettestcoin', <GiftOutlined />),
+      ]
+    }
+  }, [t, unreadProposalIds,chainId]);
 
   const onClick: MenuProps['onClick'] = (e) => {
     setCurrent(e.key);
